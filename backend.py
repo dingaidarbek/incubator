659fa199cb6f7ab6965b1415638ac2e1c4c7bc02
Dingmukhammed Aidarbek <dingaidarbek@gmail.com>
2024-05-07 13:43:28 +0400

    improved registration

--- backend.py
+++ backend.py
@@ -14,21 +14,43 @@ def registration():
 
         login = request.form["login"]
         password = request.form["password"]
+        repeatPassword = request.form["repeatPassword"]
+        if password == repeatPassword:
+            # userAccount = {'login': hash(login.strip()), 'password':hash(password.strip())} #with encoding
+            userAccount = {'login': login.strip(), 'password':password.strip()} # without encoding
+            with open("users.json") as users:  
+                usersJson = json.load(users)
+
+            usersJson.append(userAccount)
+
+            with open("users.json", 'w') as json_file:
+                json.dump(usersJson, json_file, indent=4, separators=(',',': '))
+
+            return redirect(url_for("user", usr=login))
+        else:
+            return "password does not match" + render_template("registration.html")
+    else:
+        return render_template("registration.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+
+        login = request.form["login"]
+        password = request.form["password"]
         userAccount = {'login': hash(login.strip()), 'password':hash(password.strip())}
 
         with open("users.json") as users:  
             usersJson = json.load(users)
 
-        usersJson.append(userAccount)
+        for user in usersJson:
+            if user["login"] == login:
+                if user["password"] == password:
+                            return redirect(url_for("user", usr=login))
 
-        with open("users.json", 'w') as json_file:
-            json.dump(usersJson, json_file, indent=4, separators=(',',': '))
-
-        return redirect(url_for("user", usr="dimash"))
-    
     else:
-        return render_template("registration.html")
-
+        return render_template("login.html")
+    
 @app.route("/<usr>")
 def user(usr):
     return f"<h1>{usr}</h1>"
