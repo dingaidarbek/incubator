08a6b8012592d79d30250156472ba4d0f7bdbaeb
Dingmukhammed Aidarbek <dingaidarbek@gmail.com>
2024-05-08 23:34:27 +0400

    changes

--- app.py
+++ app.py
@@ -134,15 +134,8 @@ def album(albumID):
 
 
 def str_to_dict(string):
-    # remove the curly braces from the string
     string = string.strip('{}')
- 
-    # split the string into key-value pairs
     pairs = string.split(', ')
- 
-    # use a dictionary comprehension to create
-    # the dictionary, converting the values to
-    # integers and removing the quotes from the keys
     return {key[1:-2]: int(value) for key, value in (pair.split(': ') for pair in pairs)}
 
 @app.route('/account')
